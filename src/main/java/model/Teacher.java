f9f9d3c5f0439f4a4f35208c74a49ca1c94b71ba
Twincasper <justinwatson651@gmail.com>
2024-12-18 11:34:40 -0500

    Added address class to model package and mapping in hibernate config

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -55,8 +55,10 @@
 package model;
 import jakarta.persistence.*;
 import model.Department;
+import model.Address;
 import java.io.Serial;
 import java.io.Serializable;
+
 @Entity
 @Table
 public class Teacher implements Serializable {
@@ -67,6 +69,18 @@ public class Teacher implements Serializable {
     private int teacherId;
     private String salary;
     private String teacherName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
 
     public Teacher( String salary, String teacherName) {
         super();
