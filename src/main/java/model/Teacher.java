904035631ec0507a608169bcdcc2da325623e388
Twincasper <justinwatson651@gmail.com>
2024-12-18 12:17:10 -0500

    Established cohort class and many to many teacher to cohort annotation

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,66 +1,11 @@
-//package model;
-//
-//import jakarta.persistence.*;
-//
-//import java.io.Serial;
-//import java.io.Serializable;
-//
-//@Entity
-//@Table
-//public class Teacher implements Serializable {
-//    @Serial
-//    private static final long serialVersionUID = 1L;
-//    @Id
-//    @GeneratedValue( strategy=GenerationType.IDENTITY )
-//    private int teacherId;
-//    private String salary;
-//    private String TeacherName;
-//
-//    @ManyToOne
-//    private Department department;
-//    public Teacher(int teacherId, String salary, String teacherName) {
-//        super();
-//        this.teacherId = teacherId;
-//        this.salary = salary;
-//        TeacherName = teacherName; }
-//    public Teacher() {}
-//
-//    public Teacher(String s, String mHaseeb, Department dept1) {
-//    }
-//
-//    public Department getDep() {
-//        return department; }
-//    public void setDep(Department department) {
-//        this.department = department;
-//    }
-//    public int getTeacherId() {
-//        return teacherId;
-//    }
-//    public void setTeacherId(int teacherId) {
-//        this.teacherId = teacherId;
-//    }
-//    public String getSalary() {
-//        return salary;
-//    }
-//    public void setSalary(String salary) {
-//        this.salary = salary;
-//    }
-//    public String getTeacherName() {
-//        return TeacherName;
-//    }
-//    public void setTeacherName(String teacherName) {
-//        TeacherName = teacherName; }
-//}
-
 package model;
 import jakarta.persistence.*;
-import model.Department;
-import model.Address;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
-@Table
+@Table(name="Teacher")
 public class Teacher implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -70,27 +15,52 @@ public class Teacher implements Serializable {
     private String salary;
     private String teacherName;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Address address;
 
-    public Address getAddress() {
-        return address;
+    // @OneToOne(cascade = CascadeType.ALL)
+    // private Address address;
+
+    @ManyToMany(targetEntity = Cohort.class)
+    private Set<Cohort> cohort;
+
+    public Set<Cohort> getCohort() {
+        return cohort;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    public void setCohort(Set<Cohort> cohort) {
+        this.cohort = cohort;
     }
 
+    // public Address getAddress() {
+    //     return address;
+    // }
 
-    public Teacher( String salary, String teacherName) {
+    // public void setAddress(Address address) {
+    //     this.address = address;
+    // }
+
+    public Teacher(String salary, String teacherName, Set<Cohort> cohort) {
+        this.salary = salary;
+        this.teacherName = teacherName;
+    }
+
+    public Teacher(String salary, String teacherName) {
         super();
         this.salary = salary;
-        this.teacherName = teacherName;    }
+        this.teacherName = teacherName;
+    }
+
     public Teacher() {}
+
     public Teacher(String salary, String teacherName, Department department) {
         this.salary = salary;
         this.teacherName = teacherName;
     }
+
+//    public Teacher(String salary, String teacherName) {
+//        this.salary = salary;
+//        this.teacherName = teacherName;
+//    }
+
     public int getTeacherId() {
         return teacherId;
     }
@@ -109,4 +79,3 @@ public class Teacher implements Serializable {
     public void setTeacherName(String teacherName) {
         this.teacherName = teacherName;    }
 }
-
